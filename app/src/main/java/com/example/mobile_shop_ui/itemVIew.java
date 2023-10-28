f51ac334ef4567b613748ecf6b640f34437e0882
Bharat Chaudhari <bharat.sc01@gmail.com>
2023-10-28 19:33:26 +0530

    Mobile Shop Application

--- app/src/main/java/com/example/mobile_shop_ui/itemVIew.java
+++ app/src/main/java/com/example/mobile_shop_ui/itemVIew.java
@@ -14,26 +14,26 @@ import android.widget.Toast;
 
 public class itemVIew extends AppCompatActivity {
 
-    TextView orignalVal,emi;
+//    TextView orignalVal,emi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_view);
 
-        orignalVal = findViewById(R.id.orignalVal);
-        emi = findViewById(R.id.emi);
-        
-        emi.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(itemVIew.this, "okkk", Toast.LENGTH_SHORT).show();
-            }
-        });
+//        orignalVal = findViewById(R.id.orignalVal);
+//        emi = findViewById(R.id.emi);
+//
+//        emi.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(itemVIew.this, "okkk", Toast.LENGTH_SHORT).show();
+//            }
+//        });
 
 //        SpannableString content = new SpannableString("Content");
 //        content.setSpan(new UnderlineSpan(),0,content.length(),0);
 //        orignalVal.setText("20,200");
-        orignalVal.setPaintFlags(orignalVal.getPaintFlags()|Paint.STRIKE_THRU_TEXT_FLAG);
+//        orignalVal.setPaintFlags(orignalVal.getPaintFlags()|Paint.STRIKE_THRU_TEXT_FLAG);
     }
 }
